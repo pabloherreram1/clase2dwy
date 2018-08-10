fa10163eb31792af8fe5649704de083ea8b6ab22
Pablo Herrera <pabloherreram1@hotmail.com>
2018-08-10 12:52:49 -0400

    Primersitio

---
+++ PrimerSitio/js/rutinas.js
@@ -0,0 +1,32 @@
+function mensaje(){
+    alert('Hola Mundo');
+}
+function formulario(){
+    var nombre=document.getElementById("txtNom").value;
+    var rut=document.getElementById("txtRut").value;
+    var largo=rut.length;
+    if (largo!=8) {
+        alert("rut mal ingresado");
+        return;
+    }
+    var suma=0;var mult=3;
+    for (var index = 0; index < 8; index++) {
+        var caracter=rut.substring(index,index+1);
+        suma=suma+(parseInt(caracter)*mult);
+        mult--;
+        if (mult==1) {
+            mult=7;
+        }        
+        
+    }
+    var resto =(suma % 11);
+    var dv=11-resto;
+    if (dv==10){
+        alert("k");return;
+
+    }
+    if(dv==11){
+        alert("0");return;
+    }
+    alert(dv);
+}
